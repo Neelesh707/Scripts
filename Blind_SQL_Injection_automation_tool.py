230b2f6ea48b51c6a8a3d3a1d5bdd1432d7d267b
Neelesh Pandey <72847037+Neelesh707@users.noreply.github.com>
2026-03-26 10:08:38 +0530

    Update and rename payload.py to Blind_SQL_Injection_automation_tool.py

--- Blind_SQL_Injection_automation_tool.py
+++ Blind_SQL_Injection_automation_tool.py
@@ -1,3 +1,4 @@
+#This script is a Python-based Blind SQL Injection automation tool. It is used in web security testing / CTF challenges / penetration testing labs to extract sensitive data (like passwords) from a vulnerable web application.
 import requests
 url = 'https://0aa900d803ddb13681fb024b00d7002f.web-security-academy.net/filter?category=Gifts'
 
